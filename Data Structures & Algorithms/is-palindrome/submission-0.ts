a0a7aa3bf596a821c55201b2f318fc9d8190cc44
Obembe Eyitayo David <160131025+davistar21@users.noreply.github.com>
2026-05-26 14:38:10 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        s = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+        let lP = 0;
+        let rP = s.length - 1;
+        while (rP > lP) {
+            if (s[lP] !== s[rP]) return false;
+            else {
+                lP++;
+                rP--;
+                continue;
+            }
+        }
+        return true
+    }
+}
