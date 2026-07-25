cb3962de251fba312aa4e1e1e3e2c33ab0a8878a
Obembe Eyitayo David <160131025+davistar21@users.noreply.github.com>
2026-07-25 19:25:26 +0100

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode* current = root;
+
+        while (!st.empty() || current != nullptr) {
+            if (current != nullptr) {
+                result.push_back(current->val);
+                if (current->right) {
+                    st.push(current->right);
+                } 
+                // else current = nullptr;
+                current = current->left;
+                
+            } else {
+                current = st.top();
+                st.pop();
+                
+            }
+        }
+
+        return result;
+    }
+};
